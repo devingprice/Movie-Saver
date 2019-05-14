76367a7c5caab458692d98fd2da75bb61eb4cf6f
Dylan Wright <wright.dylan5@gmail.com>
2019-05-14 14:43:31 -0400

    html routes, post/put/delete watched and wish, and models

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,4 +21,50 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+
+  //WATCHEDLIST
+
+  //Get all examples
+  app.get("/api/watchedlist", function(req, res) {
+    db.WatchedList.findAll({}).then(function(dbWatchedList) {
+      res.json(dbWatchedList);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/watchedList", function(req, res) {
+    db.WatchedList.create(req.body).then(function(dbWatchedList) {
+      res.json(dbWatchedList);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/watchedList/:id", function(req, res) {
+    db.WatchedList.destroy({ where: { id: req.params.id } }).then(function(dbWatchedList) {
+      res.json(dbWatchedList);
+    });
+  });
+
+  //WISHLIST
+
+  //Get all examples
+  app.get("/api/wishlist", function(req, res) {
+    db.WishList.findAll({}).then(function(dbWishList) {
+      res.json(dbWishList);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/wishList", function(req, res) {
+    db.WishList.create(req.body).then(function(dbWishList) {
+      res.json(dbWishList);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/wishList/:id", function(req, res) {
+    db.WishList.destroy({ where: { id: req.params.id } }).then(function(dbWishList) {
+      res.json(dbWishList);
+    });
+  });
 };
