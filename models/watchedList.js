613ad745eaf625b8e392aca37ec0f00b6eec26d0
devingprice <devingprice@gmail.com>
2019-05-15 13:03:28 -0400

    Add associations to models

--- models/watchedList.js
+++ models/watchedList.js
@@ -2,5 +2,14 @@ module.exports = function(sequelize, DataTypes) {
   var WatchedList = sequelize.define("WatchedList", {
     imdbID: DataTypes.STRING
   });
+
+  WatchedList.associate = function(models) {
+    WatchedList.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
   return WatchedList;
 };
--- models/wishList.js
+++ models/wishList.js
@@ -2,5 +2,14 @@ module.exports = function(sequelize, DataTypes) {
   var WishList = sequelize.define("WishList", {
     imdbID: DataTypes.STRING
   });
+
+  WishList.associate = function(models) {
+    WishList.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
   return WishList;
 };
