06431cd43b169b1fef3d3e9eb0a18621d33da46a
Cody Swank <cody.swank1@gmail.com>
2019-05-16 12:37:42 -0400

    trending slider

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
   $(".sidenav").sidenav();
-});
 
-$(document).ready(function() {
   $(".modal").modal();
+
+  // eslint-disable-next-line no-unused-vars
+  var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    initialSlide: 2,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true
+    },
+    pagination: {
+      el: ".swiper-pagination"
+    }
+  });
 });
