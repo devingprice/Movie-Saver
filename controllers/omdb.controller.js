b46dbea64fe4010b7dc5d473946fb1c3375ee24a
courtney manery <courtneymmanery@gmail.com>
2019-05-16 22:53:23 -0400

    testing video trailer

--- controllers/omdb.controller.js
+++ controllers/omdb.controller.js
@@ -52,7 +52,7 @@ function requestMovieDbListIfNotCached(varName, reqPath, cb) {
 }
 
 module.exports = {
-  searchMovie: function(query, cb) {
+  searchMovie: function (query, cb) {
     var requestUrl = movieDbUrl + "search/movie";
     axios
       .get(requestUrl, {
@@ -72,7 +72,7 @@ module.exports = {
         console.log(error);
       });
   },
-  movieById: function(id, cb) {
+  movieById: function (id, cb) {
     var requestUrl = movieDbUrl + "movie/" + id;
     axios
       .get(requestUrl, {
@@ -89,24 +89,137 @@ module.exports = {
         console.log(error);
       });
   },
-  trending: function(cb) {
+  trending: function (cb) {
     var reqPath = "trending/movie/week";
     var varName = "trending";
     requestMovieDbListIfNotCached(varName, reqPath, cb);
   },
-  upcoming: function(cb) {
+  upcoming: function (cb) {
     var reqPath = "movie/upcoming";
     var varName = "upcoming";
     requestMovieDbListIfNotCached(varName, reqPath, cb);
   },
-  nowPlaying: function(cb) {
+  nowPlaying: function (cb) {
     var reqPath = "movie/now_playing";
     var varName = "nowPlaying";
     requestMovieDbListIfNotCached(varName, reqPath, cb);
   },
-  topRated: function(cb) {
+  topRated: function (cb) {
     var reqPath = "movie/top_rated";
     var varName = "topRated";
     requestMovieDbListIfNotCached(varName, reqPath, cb);
   }
 };
+
+// This route then hands the data it receives to handlebars so index can be rendered.
+// router.get('/', function (req, res) {
+//   db.Movie.findAll({
+//     order: 'movie_name ASC'
+
+//   }).then(function (data) {
+//     var hbsObject = {
+//       movies: data
+//     };
+//     res.render('index', hbsObject);
+//   });
+// });
+
+// router.get('/year', function (req, res) {
+//   db.Movie.findAll({
+//     order: 'movie_year DESC'
+
+//   }).then(function (data) {
+//     var hbsObject = {
+//       movies: data
+//     };
+//     res.render('index', hbsObject);
+//   });
+// });
+
+// router.get('/rating', function (req, res) {
+//   db.Movie.findAll({
+//     order: 'movie_ratingImdb DESC'
+
+//   }).then(function (data) {
+//     var hbsObject = {
+//       movies: data
+//     };
+//     res.render('index', hbsObject);
+//   });
+// });
+
+// // testing trailer 
+// request(queryUrl, function (error, response, body) {
+
+
+//   if (!error && JSON.parse(body).Response !== 'False') {
+//     console.log(JSON.parse(body));
+
+//     var imdbId = JSON.parse(body).imdbID;
+
+//     console.log(imdbId);
+
+//     var videos = "";
+
+//     var options = {
+//       method: 'GET',
+//       url: 'https://api.themoviedb.org/3/movie/' + imdbId + '/videos',
+//       body: '{}'
+//     };
+
+//     request(options, function (error, response, result) {
+
+//       if (error) res.redirect('/');
+
+
+//       // if (error) {
+//       //     alert("Seems to be a problem with your input. Please try again");
+//       //     //res.redirect('/');
+//       // } else {
+//       if (!JSON.parse(result).results) {
+//         // res.send('SOMETHING WENT WRONG');
+//         res.redirect('/')
+//       } else {
+//         videos = JSON.parse(result).results[0].key;
+//         console.log(videos);
+//         db.Movie.create({
+//           movie_name: JSON.parse(body).Title,
+//           movie_poster: JSON.parse(body).Poster,
+//           movie_genre: JSON.parse(body).Genre,
+//           movie_time: JSON.parse(body).Runtime,
+//           movie_plot: JSON.parse(body).Plot,
+//           movie_director: JSON.parse(body).Director,
+//           movie_actors: JSON.parse(body).Actors,
+//           movie_year: JSON.parse(body).Year,
+//           movie_trailer: videos,
+//           movie_ratingImdb: JSON.parse(body).Ratings[0].Value,
+//           movie_ratingRotten: JSON.parse(body).Ratings[1].Value
+
+//         }).then(function () {
+//           res.redirect('/');
+//         });
+
+//       }
+//     });
+
+//   } else {
+//     console.log("Oops...something went wrong with you movie search. Try again...");
+//     res.redirect('/');
+//   }
+// });
+
+// // Deleting a movie
+
+// router.delete('/api/new/delete/:id', function (req, res) {
+
+//   var ID = req.params.id;
+
+//   db.Movie.destroy({
+//     where: { id: ID }
+//   }).then(function () {
+//     res.redirect('/');
+//   });
+// });
+
+// // Export routes for server.js.
+// module.exports = router;
